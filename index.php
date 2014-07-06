689f336514899286d121b51629342f4ab1b4cd6e
Ethan Wortzman <ethanwortzman@g.harvard.edu>
2014-07-05 21:02:44 -0400

    added Bootstrap framework

--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>xkcd Password Generator</title>
-	<link rel="stylesheet" type="text/css" href="css/default.css">
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>xkcd Password Generator</title>
 
-	<?php require "logic.php" ?>
+  <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
+  <?php require "logic.php" ?>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-	<form action="." method="POST">
-		<input type="text" id="password" value="<?php pw_gen(); ?>" /> <br>
-		<input type="submit" value="Generate" />
-	</form>
+
+  
+
+  <form action="." method="POST">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="input-group">
+          <input type="text" class="form-control" id="password" value="<?php pw_gen() ?>">
+          <span class="input-group-btn">
+            <button class="btn btn-default" type="submit">
+              <span class="glyphicon glyphicon-circle-arrow-right"></span>
+            </button>
+          </span>
+        </div><!-- /input-group -->
+      </div><!-- /.col-lg-6 -->
+    </div><!-- /.row -->
+  </form>
 </body>
 </html>
